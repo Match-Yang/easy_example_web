09c63ce0457802e0b9ddcefa57a54381a1860d86
zhaowei <choui20016514@gmail.com>
2022-05-24 18:08:41 +0800

    🐛  remove configuration

--- public/firebase.ts
+++ public/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBVQe2tZ-LieqpPJ7lYV1UgFOKpFaALVwg",
-  authDomain: "choui-zego-call.firebaseapp.com",
-  projectId: "choui-zego-call",
-  storageBucket: "choui-zego-call.appspot.com",
-  messagingSenderId: "342421168483",
-  appId: "1:342421168483:web:461ecd1a46fc0945553356",
-  measurementId: "G-Y62X594ZRM",
+  apiKey: "xxxxxxx",
+  authDomain: "xxxxxxx",
+  projectId: "xxxxxxx",
+  storageBucket: "xxxxxxx",
+  messagingSenderId: "xxxxxxx",
+  appId: "xxxxxxx",
+  measurementId: "xxxxxxx",
 };
 
 // Initialize Firebase
